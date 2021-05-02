5497a12daf1251dc8658552120084b8ba6e63c42
Hardik284007 <78480267+Hardik284007@users.noreply.github.com>
2021-05-02 12:56:47 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,230 @@
+onEvent("companychooser", "click", function( ) {
+  playSound("assets/category_app/snap.mp3", false);
+});
+onEvent("testbutton", "click", function( ) {
+  playSound("assets/category_app/app_menu_button_6.mp3", false);
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Samsung") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Apple") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Infinix") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Oppo") {
+    setText("informationarea", "It's chinese!");
+    setProperty("informationarea", "text-color", rgb(255,0,0));
+    playSpeech("It's chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Vivo") {
+    playSpeech("It's chinese!", "male", "हिन्दी");
+    setText("informationarea", "It's chinese!");
+    setProperty("informationarea", "text-color", rgb(255,0,0));
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Xiaomi or Redmi or Mi") {
+    setText("informationarea", "It's chinese!");
+    setProperty("informationarea", "text-color", rgb(255,0,0));
+    playSpeech("It's chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Asus") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Realme") {
+    setText("informationarea", "It's chinese!");
+    setProperty("informationarea", "text-color", rgb(255,0,0));
+    playSpeech("It's chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Lenovo") {
+    setText("informationarea", "It's chinese!");
+    setProperty("informationarea", "text-color", rgb(255,0,0));
+    playSpeech("It's chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Motorola") {
+    setText("informationarea", "It's chinese!");
+    setProperty("informationarea", "text-color", rgb(255,0,0));
+    playSpeech("It's chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Nokia") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Haier") {
+    playSpeech("It's chinese!", "male", "हिन्दी");
+    setText("informationarea", "It's chinese!");
+    setProperty("informationarea", "text-color", rgb(255,0,0));
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "LG") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Voltas") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Huawei") {
+    playSpeech("It's chinese!", "male", "हिन्दी");
+    setText("informationarea", "It's chinese!");
+    setProperty("informationarea", "text-color", rgb(255,0,0));
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Honor") {
+    playSpeech("It's chinese!", "male", "हिन्दी");
+    setText("informationarea", "It's chinese!");
+    setProperty("informationarea", "text-color", rgb(255,0,0));
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Poco") {
+    playSpeech("It's chinese!", "male", "हिन्दी");
+    setText("informationarea", "It's chinese!");
+    setProperty("informationarea", "text-color", rgb(255,0,0));
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "HP(Hewlett-Packard)") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Acer") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Sony") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Intex") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Dell") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Godrej") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Boat") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Bose") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Gionee") {
+    playSpeech("It's chinese!", "male", "हिन्दी");
+    setText("informationarea", "It's chinese!");
+    setProperty("informationarea", "text-color", rgb(255,0,0));
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Panasonic") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Bajaj") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Aquaguard") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Kent") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Zebronics") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
+onEvent("testbutton", "click", function( ) {
+  if (getText("companychooser") == "Logitech") {
+    setText("informationarea", "It's not chinese!");
+    setProperty("informationarea", "text-color", rgb(0,255,0));
+    playSpeech("It's not chinese!", "male", "हिन्दी");
+  }
+});
